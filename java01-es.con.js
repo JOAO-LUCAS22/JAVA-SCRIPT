af5b4a39f996a14a47c5eec2fb96d73e42eec23d
vladimir <vladimir.menezes@ba.estudante.senai.br>
2026-03-05 14:44:23 -0300

    java01es.con..js

---
+++ java01-es.con.js
@@ -0,0 +1,14 @@
+// ESTRUTURAS CONDICIONAIS
+
+const idade = 15
+
+if (idade >= 18) {
+    console.log('Você é maior de idade.')
+} else if (idade >= 12) {
+    console.log('Você é um adolescente.')
+} else if (idade >= 2){
+    console.log('Você é uma criança.')
+}
+else {
+    console.log('Você é um bebê.')
+}
