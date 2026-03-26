55c8719e6e61da8cf90b8d8af2b2bd75d3a1a5a5
vladimir <vladimir.menezes@ba.estudante.senai.br>
2026-03-26 16:54:10 -0300

    ok

---
+++ projeto/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>PROJETO LAVAGEM DE REAIS</h1>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur sapiente animi eius ipsa, autem dolores veniam quis officiis laborum, cupiditate at ipsam sit soluta nisi nam ratione. Facere, quos laboriosam!
+    Dolore odio blanditiis, dolorem qui assumenda molestiae doloremque quaerat exercitationem quia rerum pariatur unde vitae inventore earum totam voluptate temporibus iste autem voluptates tempore? Expedita provident ea saepe ratione doloremque!
+    Aliquam excepturi ut dolorem eaque adipisci sint soluta maiores architecto aut suscipit odio, quo similique laboriosam aperiam commodi cupiditate at inventore officia iusto eveniet dolores odit culpa nisi id? Ad!
+    Ullam cumque corrupti non, deserunt ipsam ab! Accusantium, enim. Praesentium ex minus eos quis eaque nulla! Perspiciatis commodi dolorum voluptatum doloremque atque porro. Exercitationem, accusantium inventore ad autem aut culpa?
+    Mollitia magni, alias assumenda molestiae repudiandae enim voluptates placeat deleniti ea inventore rem saepe quod nulla porro ratione doloribus perferendis natus, sunt officia delectus laudantium iste dolore. Libero, laudantium blanditiis!
+    Adipisci error ratione laborum atque deserunt fuga rerum repellendus. Quibusdam aliquam veniam impedit nisi porro doloribus expedita, natus dignissimos nobis ab facere praesentium esse maxime amet repellendus blanditiis ut officiis.
+    Unde ipsam sapiente error inventore, assumenda perferendis dolores dolorum molestias cumque praesentium ducimus. Sunt inventore id corrupti quisquam at, beatae nam, illo doloribus voluptatibus incidunt, odit ducimus atque nesciunt deleniti?
+    Dignissimos nam, at quas fugit minus nulla voluptatum quae alias autem veritatis. Officia quaerat adipisci amet quo minus recusandae distinctio doloribus necessitatibus! Velit corporis voluptatibus laboriosam incidunt sint, adipisci mollitia!
+    Quibusdam doloremque deserunt quia aperiam blanditiis harum maiores dolorem consectetur autem, quod aspernatur necessitatibus eligendi temporibus id, veritatis quo eius nemo maxime, cum ducimus quaerat odio at! Quisquam, atque in.
+    Dolor quam dicta accusamus illum facere commodi libero incidunt odio omnis? Officia natus quisquam adipisci quaerat aliquam autem quae cum, facere dolore ut! Quia itaque illo minima distinctio odit veritatis.
+    
+    </>
+  )
+}
+
+export default App
